d3f5ded7a7f483ee2b0ad15251c29aeba51e24ed
sunchaoyuan <824922901@qq.com>
2018-05-05 16:42:17 +0800

    spring生命周期测试

---
+++ src/com/scy/test/Test.java
@@ -0,0 +1,37 @@
+
+/**
+ * Date:2018年5月5日上午11:22:04
+ * Copyright (c) 2018, www.bwbroad.com All Rights Reserved.
+ *
+*/
+
+package com.scy.test;
+
+import javax.security.auth.DestroyFailedException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.scy.spring.Dog;
+
+/**
+ * Description: TODO <br/>
+ * Date:     2018年5月5日 上午11:22:04 <br/>
+ * @author   sunchaoyuan	 
+ */
+public class Test {
+	public static void main(String[] args) throws DestroyFailedException {
+		/**
+		 * spring生命周期
+		 * 1.beanfacotrypostprocessor用法
+		 * 2.beannameaware
+		 * 3.beanpostprocessor
+		 * 4.InitializingBean
+		 * 5.销毁 predestroy
+		 */
+		ApplicationContext ac = new ClassPathXmlApplicationContext("/application.xml");
+		Dog  dog = (Dog) ac.getBean("dog");
+		dog.sing();
+	}
+}
+
