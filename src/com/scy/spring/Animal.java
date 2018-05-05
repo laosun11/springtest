fabbb586d03b4ec77738863a1250e51005e103bd
sunchaoyuan <824922901@qq.com>
2018-05-05 17:58:57 +0800

    类生命周期

---
+++ src/com/scy/spring/Animal.java
@@ -0,0 +1,19 @@
+
+/**
+ * Date:2018年5月5日下午12:05:58
+ * Copyright (c) 2018, www.bwbroad.com All Rights Reserved.
+ *
+*/
+
+package com.scy.spring;
+/**
+ * Description: TODO <br/>
+ * Date:     2018年5月5日 下午12:05:58 <br/>
+ * @author   sunchaoyuan	 
+ */
+public interface Animal {
+	public String name=null;
+	public Integer age=null;
+	public void sing();
+}
+
---
+++ src/com/scy/test/ClassCycleTest.java
@@ -0,0 +1,38 @@
+
+/**
+ * Date:2018年5月5日下午4:44:55
+ * Copyright (c) 2018, www.bwbroad.com All Rights Reserved.
+ *
+*/
+
+package com.scy.test;
+/**
+ * Description: TODO <br/>
+ * Date:     2018年5月5日 下午4:44:55 <br/>
+ * @author   sunchaoyuan	 
+ */
+public class ClassCycleTest {
+	private String name = "zhangsan";
+	private static Integer age = 10;
+	
+	public ClassCycleTest() {
+	}
+	
+	static{
+		String a = "haha";
+	}
+	
+	public static void main(String[] args) {
+		/*类生命周期
+		 * 1.加载
+		 * 2.验证
+		 * 3.准备（给类属性分配空间）
+		 * 4.解析(符号变量转化为引用变量)
+		 * 4.初始化
+		 * clinit
+		 * init
+		 */
+		ClassLoader classLoader = ClassCycleTest.class.getClassLoader();
+	}
+}
+
